984faed8fba84e0884925a4cc834da91520024fe
meimoon1 <melevelyn69@gmail.com>
2021-09-29 16:11:07 -0300

    Criacando dois objetos

--- Atividade_conceitos/Atp15.java
+++ Atividade_conceitos/Atp15.java
@@ -2,6 +2,17 @@ package Atividade_conceitos;
 public class Atp15{
     public static void main(String[] args) {
         Pessoa p1 = new Pessoa();
+        p1.nome = "Melissa";
+        p1.sobrenome = "Matos";
+        p1.idade = 23;
+
         Pessoa p2 = new Pessoa();
+        p2.nome = "Frodo";
+        p2.sobrenome = "Bolseiro";
+        p2.idade = 33;
+
+        
+
+        
     }
 }
