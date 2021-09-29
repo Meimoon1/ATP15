e99515183c1d0363775c0f8ba62d811025d739c8
meimoon1 <melevelyn69@gmail.com>
2021-09-29 16:18:28 -0300

    Imprimindo primeiro objeto

--- Atividade_conceitos/Atp15.java
+++ Atividade_conceitos/Atp15.java
@@ -1,16 +1,25 @@
 package Atividade_conceitos;
 public class Atp15{
     public static void main(String[] args) {
+        // Primeiro objeto da classe pessoa
         Pessoa p1 = new Pessoa();
         p1.nome = "Melissa";
         p1.sobrenome = "Matos";
         p1.idade = 23;
 
+        // Segundo objeto da classe pessoa
         Pessoa p2 = new Pessoa();
         p2.nome = "Frodo";
         p2.sobrenome = "Bolseiro";
         p2.idade = 33;
 
+        //Imprimindo Primeira pessoa
+        System.out.println("---------Pessoa 1---------");
+        System.out.println("Nome: "+p1.nome);
+        System.out.println("Sobrenome :"+p1.sobrenome);
+        System.out.println("Idadade: "+p2.idade);
+
+        
         
 
         
